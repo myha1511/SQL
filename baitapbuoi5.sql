5ba23ce3d1b9114741ae03e8e06fb00ce98f17ab
myha1511 <myhanh151198@gmail.com>
2024-06-06 23:51:22 +0700

    Update baitapbuoi5.sql

--- baitapbuoi5.sql
+++ baitapbuoi5.sql
@@ -1,9 +1,11 @@
 /* Câu 1: https://www.hackerrank.com/challenges/weather-observation-station-3*/
-
-
+select distinct city from station 
+where id % 2=0
 
 /* ex2: hackerrank-weather-observation-station-4.*/
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) 
 FROM STATION;
 
-/*ex3: https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true*/
+/*ex4: datalemur-alibaba-compressed-mean.*/
+SELECT round(cast(sum(item_count*order_occurrences)/sum(order_occurrences)as decimal),1) as mean 
+FROM items_per_order
