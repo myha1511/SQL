180a903c68a9f91b196928a8a99eaa333811d2da
myha1511 <myhanh151198@gmail.com>
2024-06-07 00:25:57 +0700

    Update baitapbuoi5.sql

--- baitapbuoi5.sql
+++ baitapbuoi5.sql
@@ -21,3 +21,10 @@ FROM posts
 where  post_date >='2021-01-01' and post_date <='2022-01-01'
 group by (user_id)
 having count (post_id) >=2
+
+/*ex7:datalemur-cards-issued-difference.*/
+SELECT card_name , (max(issued_amount)- min(issued_amount)) as difference
+FROM monthly_cards_issued
+GROUP BY (card_name)
+ORDER BY difference DESC
+
