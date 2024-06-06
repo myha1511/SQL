cf43ea289b2b057bb48e350fddca8501d3b798ca
myha1511 <myhanh151198@gmail.com>
2024-06-06 20:00:06 +0700

    baitapbuoi5.sql

---
+++ baitapbuoi5.sql
@@ -0,0 +1,9 @@
+/* Câu 1: https://www.hackerrank.com/challenges/weather-observation-station-3*/
+
+
+
+/* ex2: hackerrank-weather-observation-station-4.*/
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) 
+FROM STATION;
+
+/*ex3: https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true*/
